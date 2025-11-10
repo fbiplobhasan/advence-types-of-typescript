53b0d42cc579cf47c2e782b436c1d53681c2bb6e
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-11-10 22:06:40 +0600

    aypeAssertion

---
+++ typeAssertion.ts
@@ -0,0 +1,28 @@
+// 01
+// let anything: any;
+
+// anything = 1234;
+
+// (anything as number ).
+
+// 02
+
+// let anything: any;
+
+// anything = 'Mezba';
+
+// (anything as string ).
+
+const kgToGMConverter = (input: string | number) => {
+if(typeof input === 'number'){
+    return input * 1000;
+} else if(typeof input === 'string'){
+    const [value] = input.split(" ")
+    return `Converted output is: ${Number(value) * 1000}`;
+}
+}
+
+const result1 = kgToGMConverter(20);
+console.log({result1});
+const result2 = kgToGMConverter('2 kg')
+console.log({result2});
