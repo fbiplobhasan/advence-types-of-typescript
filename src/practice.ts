c1f31989502a9d7ab8b2a6bcaca51a937ef8ea2d
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-11-15 12:50:41 +0600

    Static in typescript

--- src/practice.ts
+++ src/practice.ts
@@ -583,15 +583,28 @@
 // *Static
 
 class Counter {
-  count: number = 0;
+  static count: number = 0;
 
-  increment() {
-    return (this.count = this.count + 1);
+  static increment() {
+    return (Counter.count = Counter.count + 1);
   }
-  decrement() {
-    return (this.count = this.count - 1);
+  static decrement() {
+    return (Counter.count = Counter.count - 1);
   }
 }
 
-const instance1 = new Counter();
-console.log(instance1.increment());
+// const instance1 = new Counter();
+console.log(Counter.increment());
+console.log(Counter.increment());
+console.log(Counter.increment());
+
+// const instance2 = new Counter()
+console.log(Counter.increment());
+console.log(Counter.increment());
+console.log(Counter.increment());
+
+
+// const instance3 = new Counter()
+console.log(Counter.increment());
+console.log(Counter.increment());
+console.log(Counter.increment());
